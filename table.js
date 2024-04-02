1c4730037895e59da95454df02ef0162261ca648
abdulazeez003 <sabdulazeez133@gmail.com>
2024-04-02 02:39:27 -0700

    update

---
+++ table.js
@@ -0,0 +1,10 @@
+const body = document .querySelector['body'];
+const  table = document .querySelector['table'];
+
+const colors =[ 'blue', 'green', 'red', 'pink',];
+
+h1.addEventlistener('click',  () => {
+      const colors = colors { math.floor ( math.random()* colors,length) };
+
+      body.style.backgrandcolor =color:
+} )
